b8b3cda12d5d86b830e69ad38cb69fb114c246ac
sruthijaganmohan <sruthi12jagan@gmail.com>
2023-03-30 13:09:18 +0530

    mid commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from "react";
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState} from "react";
 import * as tf from "@tensorflow/tfjs";
 import './App.css';
 import './model/model.json';
-import { model } from "@tensorflow/tfjs";
+
+
 
 const App = () => {
   const [inputImage, setInputImage] = useState(null);
-  const [models, setModels] = useState();
-
+  const [model, setModel] = useState();
 
   const loadModel = async () => {
-    // const def = new model('./model.json');
     try {
-      // await def.save('localstorage://model.json');
-      const gansModel = await tf.loadLayersModel('model.json');
-      // const gansModel = await tf.loadLayersModel('localstorage://model.json');
-      setModels(gansModel);
-      console.log(gansModel);
+      const gansModel = await tf.loadLayersModel('http://localhost:8080/model.json');
+      setModel(gansModel);console.log(gansModel);
       console.log("set loaded Model");
     } 
     catch (err) {
@@ -27,22 +23,62 @@ const App = () => {
   }
 
   const preprocessImage = (inputImage) => {
-    let img = new Image(inputImage);
-    img.width = 100;
-    img.height = 100;
-    let tensor = tf.browser.fromPixels(img);
-    const resized = tf.image.resizeBilinear(tensor, [32, 32]).toFloat();
-    const offset = tf.scalar(255.0);
-    const normalized = tf.scalar(1.0).sub(resized.div(offset));
-    const lri = normalized.expandDims(0);
-    console.log(lri);
-    superResolve(lri);
-  }
+    const img = new Image(inputImage);
+    img.width = 32;
+    img.height = 32;
+    // let offset = tf.scalar(255.0)
+    // let lri = tf.browser.fromPixels(img).toFloat().sub(offset).div(offset).expandDims();
+    // console.log(lri);
+    // superResolve(model, lri);
+    img.onload = () => {
+      const offset = tf.scalar(255.0);
+      const lri = tf.browser
+        .fromPixels(img)
+        .toFloat()
+        .sub(offset)
+        .div(offset)
+        .expandDims();
+      console.log(lri);
+      superResolve(model, lri);
+    };
+    img.src = URL.createObjectURL(inputImage);
+  };
 
-  const superResolve = (lri) => {
-    const prediction = models.predict(lri);
-    console.log(prediction);
-  }
+  
+  const superResolve = (model, lri) => {
+    console.log(lri);
+    const sri = model.predict([lri]);
+    console.log(sri);
+    construct(sri);
+  };
+
+  const construct = (sri) => {
+    const canvas = new OffscreenCanvas(128, 128);
+    const ctx = canvas.getContext('2d');
+    const width = sri.shape[1];
+    const height = sri.shape[2];
+    const buffer = new Uint8ClampedArray(width * height * 4);
+    const imageData = new ImageData(width, height);
+    const data = sri.dataSync();
+    var i = 0;
+    for(var y = 0; y < height; y++) {
+    for(var x = 0; x < width; x++) {
+        var pos = (y * width + x) * 4;      
+        buffer[pos  ] = data[i]             
+        buffer[pos+1] = data[i+1]           
+        buffer[pos+2] = data[i+2]           
+        buffer[pos+3] = 255;            
+        i+=3
+      }
+    }
+    imageData.data.set(buffer);
+    ctx.putImageData(imageData, 0, 0);
+    canvas.convertToBlob().then(function(blob) {
+          const url = URL.createObjectURL(blob);
+          console.log({ src: url });
+        });
+
+  };
 
   useEffect(() => {
     tf.ready().then(() => {
@@ -64,9 +100,9 @@ const App = () => {
           </div>
         )}
 
-        <div>
+      <div>
         <button onClick={() => preprocessImage(inputImage)}>super resolve</button>
-        </div>
+      </div>
 
         <input
           type="file"
