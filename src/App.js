b22e09e3ba6fd161122740201fe8f04d4f2cf164
sruthijaganmohan <sruthi12jagan@gmail.com>
2023-04-03 15:43:04 +0530

    Implemented output functionality using TensorFlow.js model

--- src/App.js
+++ src/App.js
@@ -22,63 +22,109 @@ const App = () => {
     }
   }
 
+  // const preprocessImage = (inputImage) => {
+  //   let img = document.getElementById('saturn');
+  //   img.width = 128;
+  //   img.height = 128;
+    // img.src = URL.createObjectURL(inputImage);
+    // img.addEventListener("load", function() {
+      // document.body.appendChild(img);
+      // let offset = tf.scalar(255.0);
+      // let lri = tf.browser.fromPixels(img).toFloat().sub(offset).div(offset).expandDims();
+  //     let lri = tf.browser.fromPixels(img);
+  //     console.log(lri);
+  //     console.log(lri.shape);
+  //     const data = lri.dataSync();
+  //     console.log(data);
+  //   });
+  // };
+
   const preprocessImage = (inputImage) => {
-    const img = new Image(inputImage);
+    const img = new Image();
+    img.src = URL.createObjectURL(inputImage);
     img.width = 32;
     img.height = 32;
-    // let offset = tf.scalar(255.0)
-    // let lri = tf.browser.fromPixels(img).toFloat().sub(offset).div(offset).expandDims();
-    // console.log(lri);
-    // superResolve(model, lri);
-    img.onload = () => {
-      const offset = tf.scalar(255.0);
-      const lri = tf.browser
-        .fromPixels(img)
-        .toFloat()
-        .sub(offset)
-        .div(offset)
-        .expandDims();
+    img.addEventListener("load", function() {
+      let offset = tf.scalar(255.0);
+      const lri = tf.browser.fromPixels(img).div(offset).expandDims();
       console.log(lri);
-      superResolve(model, lri);
-    };
-    img.src = URL.createObjectURL(inputImage);
-  };
+      const sri = model.predict(lri);
+      console.log(sri);
+      
+      // const tensorWithoutBatch = tf.squeeze(lri);
+      // const scaledTensor = tensorWithoutBatch.mul(255);
+      // const offsetTensor = scaledTensor.add(255);
+      // const intTensor = tf.cast(offsetTensor, 'int32');
+      // const finalTensor = tf.squeeze(intTensor);
+      const tensorWithoutBatch = tf.squeeze(sri)
+      const finalTensor = tensorWithoutBatch.mul(255);
+      // const sriRestored = sri.mul(offset).clipByValue(0, 255).div(offset);
+      // const scaledTensor = sriRestored.mul(255);
+      // const intTensor = tf.cast(scaledTensor, 'int32');
+      // const finalTensor = tf.squeeze(intTensor);
+      console.log(finalTensor);
+      const canvas = document.createElement('canvas');
+      canvas.width = 128;
+      canvas.height = 128;
+      const ctx = canvas.getContext('2d');
+      const [height, width] = finalTensor.shape;
+      const buffer = new Uint8ClampedArray(width * height * 4);
+      const imageData = new ImageData(width, height);
+      const data = finalTensor.dataSync();
+      console.log(data);
+      var i = 0;
+      for(var y = 0; y < height; y++) {
+      for(var x = 0; x < width; x++) {
+          var pos = (y * width + x) * 4;      
+          buffer[pos  ] = data[i]             
+          buffer[pos+1] = data[i+1]           
+          buffer[pos+2] = data[i+2]           
+          buffer[pos+3] = 255;            
+          i+=3
+        }
+      }
+      imageData.data.set(buffer);
+      ctx.putImageData(imageData, 0, 0);
+      const url = canvas.toDataURL();
+      console.log(url);
+    });
+  }
 
   
-  const superResolve = (model, lri) => {
-    console.log(lri);
-    const sri = model.predict([lri]);
-    console.log(sri);
-    construct(sri);
-  };
+  // const superResolve = (model, lri) => {
+  //   const sri = model.predict(lri);
+  //   console.log(sri);
+  //   construct(sri);
+  // };
 
-  const construct = (sri) => {
-    const canvas = new OffscreenCanvas(128, 128);
-    const ctx = canvas.getContext('2d');
-    const width = sri.shape[1];
-    const height = sri.shape[2];
-    const buffer = new Uint8ClampedArray(width * height * 4);
-    const imageData = new ImageData(width, height);
-    const data = sri.dataSync();
-    var i = 0;
-    for(var y = 0; y < height; y++) {
-    for(var x = 0; x < width; x++) {
-        var pos = (y * width + x) * 4;      
-        buffer[pos  ] = data[i]             
-        buffer[pos+1] = data[i+1]           
-        buffer[pos+2] = data[i+2]           
-        buffer[pos+3] = 255;            
-        i+=3
-      }
-    }
-    imageData.data.set(buffer);
-    ctx.putImageData(imageData, 0, 0);
-    canvas.convertToBlob().then(function(blob) {
-          const url = URL.createObjectURL(blob);
-          console.log({ src: url });
-        });
-
-  };
+  // const construct = (sri) => {
+  //   const canvas = document.createElement('canvas');
+  //   canvas.width = 128;
+  //   canvas.height = 128;
+  //   const ctx = canvas.getContext('2d');
+  //   const [height, width] = sri.shape;
+  //   const buffer = new Uint8ClampedArray(width * height * 4);
+  //   const imageData = new ImageData(width, height);
+  //   const data = sri.dataSync();
+  //   console.log(data);
+  //   var i = 0;
+  //   for(var y = 0; y < height; y++) {
+  //   for(var x = 0; x < width; x++) {
+  //       var pos = (y * width + x) * 4;      
+  //       buffer[pos  ] = data[i]             
+  //       buffer[pos+1] = data[i+1]           
+  //       buffer[pos+2] = data[i+2]           
+  //       buffer[pos+3] = 255;            
+  //       i+=3
+  //     }
+  //   }
+  //   imageData.data.set(buffer);
+  //   ctx.putImageData(imageData, 0, 0);
+    // const url = URL.createObjectURL(imageData);
+    // console.log({ src: url });
+  //   const url = canvas.toDataURL();
+  //   console.log(url); 
+  // };
 
   useEffect(() => {
     tf.ready().then(() => {
@@ -105,6 +151,7 @@ const App = () => {
       </div>
 
         <input
+          id="saturn"
           type="file"
           name="myImage"
           onChange={(event) => {
